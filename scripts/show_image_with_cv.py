aa5af774eff4247951e0cb2ba5f2445bafdfcdb5
Michael Karotsieris <michael.karotsieris@gmail.com>
2021-03-27 16:40:20 +0000

    add py script that accepts an image file path as an argument and displays the image

---
+++ scripts/show_image_with_cv.py
@@ -0,0 +1,20 @@
+"""
+Display an image with opencv. Press a key to exit the program
+"""
+
+import argparse
+import cv2
+  
+
+if __name__ == "__main__":
+    parser = parser = argparse.ArgumentParser(description='Show an image with opencv    ')
+    parser.add_argument("-i", "--image", help="Path of the image you want to display", type=str, required=True)
+    args = parser.parse_args()
+    print(f"Launching window for {args.image}")
+    image = cv2.imread(args.image) 
+  
+    # Output img with window name as 'image'
+    cv2.imshow('image', image)
+    # Exit on keyboard press
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
